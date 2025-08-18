ca2ac5d435c1fafe4e3d6d5dce65527a01e7e050
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-18 21:18:40 +0800

    update nav in mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import 'boxicons/css/boxicons.min.css';
 
 const Header = () => {
   return (
@@ -24,9 +24,33 @@ const Header = () => {
             </a>
         </nav>
 
-        <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full">
+        <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full hover:bg-white cursor-pointer">
             SignIn
         </button>
+
+        {/* mmobile menu button - visible only in mobile*/}
+        <button className='md:hidden text-3xl p-2 z-50'>
+            <i className="bx bx-menu"></i>
+        </button>
+
+        {/* Mobile menu - hidden by default */}
+        <div className='flex top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur-md'>
+            <nav className='flex flex-col gap-6 items-center'>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Company
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Features
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Resources
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Docs
+            </a>
+            </nav>
+        </div>
+
 
     </header>
   )
