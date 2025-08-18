5286ebc332f9abee0963a27c2518c741a7d7e51e
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-18 13:30:44 +0800

    update background img and blur effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+export default function App () {
+  return(
+    <main>
+      {/* Gradient image */}
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="gradient-img" />
 
-const App = () => {
-  return (
-    <div className='font-bold text-[50px]'>Poppins</div>
+      {/* Blur effect */}
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+    </main>
   )
 }
-
-export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-4 px-4 ig:px-20"></header>
+  )
+}
+
+export default Header
