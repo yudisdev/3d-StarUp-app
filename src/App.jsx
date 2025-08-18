c0872d9ba26928b3c16ab864ae57f5d46a201742
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-18 14:33:11 +0800

    update header in header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Header from "./components/Header"
+
 export default function App () {
   return(
     <main>
@@ -6,6 +8,8 @@ export default function App () {
 
       {/* Blur effect */}
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <Header/>
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,33 @@
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-4 px-4 ig:px-20"></header>
+    <header className="flex justify-between items-center py-4 px-4 ig:px-20">
+
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
+            Yudistira
+        </h1>
+
+        {/* desktop navigation */}
+        <nav className="hidden md:flex items-center gap-12">
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Company
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Features
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Resources
+            </a>
+            <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
+                Docs
+            </a>
+        </nav>
+
+        <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full">
+            SignIn
+        </button>
+
+    </header>
   )
 }
 
