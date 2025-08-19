45e53cad4450c320effed5ff2dc43870444150d7
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-19 09:34:38 +0800

    update logic in navbar menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
 import 'boxicons/css/boxicons.min.css';
 
 const Header = () => {
+
+    // simple function to toggle the mobile menu
+    const toggleMobileMenu = () => {
+        //get the mobile menu element
+        const mobileMenu = document.getElementById('mobileMenu')
+
+        // if it has the 'hidden' class, remove it . otherwisem, add it
+        if (mobileMenu.classList.contains('hidden')) {
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden');
+        }
+    }
+
   return (
     <header className="flex justify-between items-center py-4 px-4 ig:px-20">
 
@@ -29,12 +43,12 @@ const Header = () => {
         </button>
 
         {/* mmobile menu button - visible only in mobile*/}
-        <button className='md:hidden text-3xl p-2 z-50'>
+        <button onClick={toggleMobileMenu} className='md:hidden text-3xl p-2 z-50'>
             <i className="bx bx-menu"></i>
         </button>
 
         {/* Mobile menu - hidden by default */}
-        <div className='flex top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur-md'>
+        <div id='mobileMenu' className='hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur-md'>
             <nav className='flex flex-col gap-6 items-center'>
             <a className="text-base tracking-widertran transition-colors hover:text-gray-300 z-50" href="">
                 Company
